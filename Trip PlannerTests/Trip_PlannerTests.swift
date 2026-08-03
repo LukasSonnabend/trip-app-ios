7ac1ca8b0856fb3f1891204e6fc47a678ac6b70e
Lukas Sonnabend <lukas.sonnabend@outlook.com>
2026-08-03 18:37:53 +0200

    Initial Commit

---
+++ Trip PlannerTests/Trip_PlannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Trip_PlannerTests.swift
+//  Trip PlannerTests
+//
+//  Created by Lukas Sonnabend on 03.08.2026.
+//
+
+import Testing
+@testable import Trip_Planner
+
+struct Trip_PlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
